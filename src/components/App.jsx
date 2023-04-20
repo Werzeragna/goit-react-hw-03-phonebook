64c1944defe126288a50b18868967acfce882229
Maksym Lepikhov <Werzeragna@icloud.com>
2023-04-20 11:33:50 +0300

    React HW 3 v1.0
    Project start

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { StyledContainer, StyledSubtitle, StyledTitle } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmit = ({ name, number }) => {
+    const contact = { id: nanoid(), name, number };
+    this.state.contacts.some(
+      i =>
+        i.name.toLowerCase() === contact.name.toLowerCase() ||
+        i.number === contact.number
+    )
+      ? alert(`${name} is already in contacts`)
+      : this.setState(({ contacts }) => ({ contacts: [contact, ...contacts] }));
+  };
+
+  changeFilterInput = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  findContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <StyledContainer>
+        <StyledTitle>Phonebook</StyledTitle>
+        <ContactForm onSubmit={this.formSubmit} />
+        <StyledSubtitle>Contacts</StyledSubtitle>
+        <Filter filter={filter} changeFilterInput={this.changeFilterInput} />
+        <ContactList
+          contacts={this.findContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </StyledContainer>
+    );
+  }
+}
