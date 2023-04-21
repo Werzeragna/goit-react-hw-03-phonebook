b20492131e338b75e23b76bec685cb4c06645c9b
Maksym Lepikhov <Werzeragna@icloud.com>
2023-04-21 11:08:56 +0300

    React HW3 Phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,27 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { StyledContainer, StyledSubtitle, StyledTitle } from './App.styled';
 
+
+const LOCAL_STORAGE_KEY = 'contacts'
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const parsedContacts = JSON.parse(contacts)
+    if(parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.contacts !== prevState.contact) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state.contacts))
+    }
+  }
 
   formSubmit = ({ name, number }) => {
     const contact = { id: nanoid(), name, number };
